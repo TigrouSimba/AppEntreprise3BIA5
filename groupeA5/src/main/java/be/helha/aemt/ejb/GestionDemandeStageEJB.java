0c762731a4876b12e1d42e5436310a2b26760a12
Nevo067 <51850812+Nevo067@users.noreply.github.com>
2019-12-26 13:41:18 +0100

    ajout des DemandeStage
    ajoute Entities DemandeStage
    ajoute Dao DemandeStage
    ajoute Ejb DemandeStage

---
+++ groupeA5/src/main/java/be/helha/aemt/ejb/GestionDemandeStageEJB.java
@@ -0,0 +1,27 @@
+package be.helha.aemt.ejb;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import be.helha.aemt.dao.DemandeStageDao;
+import be.helha.aemt.entities.DemandeStage;
+
+public class GestionDemandeStageEJB  {
+	@EJB
+	private DemandeStageDao dao;
+	
+	public List<DemandeStage>findAll()
+	{
+		return dao.findAll();
+	}
+	
+	public void post(DemandeStage post)
+	{
+		dao.Add(post);
+	}
+
+	
+}
